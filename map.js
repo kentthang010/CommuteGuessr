8fe5482e16c2a7fd899f419a0a12fa3884215c78
kentthang010 <thangkent010@gmail.com>
2026-04-14 20:02:48 +0200

    try to add routing (may need to switch API)

--- map.js
+++ map.js
@@ -5,4 +5,26 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([51.5, -0.09]).addTo(map);
+var control = L.Routing.control({
+	router: L.Routing.osrmv1({
+		serviceUrl: 'https://router.project-osrm.org/route/v1',
+		// driving, foot, bike
+		profile: 'dance',
+	}),
+	waypoints: [
+		L.latLng(51.5, -0.5),
+		L.latLng(51.51, -0.12)
+	],
+	routeWhileDragging: true
+}).addTo(map);
+
+// Capture the travel time
+control.on('routesfound', function (e) {
+	var routes = e.routes;
+	var summary = routes[0].summary;
+	// summary.totalTime gives time in seconds
+	var timeInMinutes = Math.round(summary.totalTime / 60);
+	console.log('Travel time: ' + timeInMinutes + ' minutes');
+});
+
+// var marker = L.marker([51.5, -0.09]).addTo(map);
