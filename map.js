d00f20693e68799d6355aac91931bb0a25d0b934
kentthang010 <thangkent010@gmail.com>
2026-04-18 15:59:13 +0200

    change serviceUrl to openstreetmap.de which allows different modes of travel

--- map.js
+++ map.js
@@ -1,4 +1,17 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var lat = 51.5 + (Math.random() - 0.5) * 0.1;
+var lng = -0.09 + (Math.random() - 0.5) * 0.1;
+var transportModes = ['routed-car', 'routed-foot', 'routed-bike'];
+var modeoftransport = transportModes[Math.floor(Math.random() * transportModes.length)];
+
+function randomizeStart() {
+	// This function generates a random starting point around a central location (e.g., London)
+	// Later on I want it to not just be around London but also all over the world.
+	// Perhaps, a way to avoid generating points in the ocean is sufficient.
+	lat = 51.5 + (Math.random() - 0.5) * 0.1; // Random latitude around 51.5
+	lng = -0.09 + (Math.random() - 0.5) * 0.1; // Random longitude around -0.09
+}
+
+var map = L.map('map').setView([lat, lng], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	referrerPolicy: 'origin-when-cross-origin',
@@ -6,10 +19,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 var control = L.Routing.control({
+	containerClassName: 'routing-container',
 	router: L.Routing.osrmv1({
-		serviceUrl: 'https://router.project-osrm.org/route/v1',
-		// driving, foot, bike
-		profile: 'dance',
+		// routed-bike, routed-foot and routed-car services do
+		serviceUrl: 'https://routing.openstreetmap.de/' + modeoftransport + '/route/v1',
 	}),
 	waypoints: [
 		L.latLng(51.5, -0.5),
